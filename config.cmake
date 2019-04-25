7585fd6cc76fcf08da06410610e54437561826a0
19317362 <19317362@qq.com>
2019-04-25 23:38:20 +0800

    v1 works ok

---
+++ config.cmake
@@ -0,0 +1,24 @@
+# config.cmake
+
+# hunter_config(
+#   Boost
+#   VERSION ${HUNTER_Boost_VERSION}
+#   CMAKE_ARGS
+#   PYTHON_VERSION=3.6.7
+# )
+
+# Add 'NO_BZIP2=1' to the b2 build of iostreams library,
+# i.e. `b2 -s NO_BZIP2=1`
+# hunter_config(
+#     Boost
+#     VERSION ${HUNTER_Boost_VERSION}
+#     CMAKE_ARGS IOSTREAMS_NO_BZIP2=1
+# )
+
+# hunter_config(
+#     Boost
+#     VERSION ${HUNTER_Boost_VERSION}
+#     CMAKE_ARGS
+#     CONFIG_MACRO=BOOST_REGEX_MATCH_EXTRA;BOOST_MPL_CFG_NO_PREPROCESSED_HEADERS
+#     CONFIG_MACRO_BOOST_MPL_LIMIT_LIST_SIZE=3
+# )
--- simple.cpp
+++ simple.cpp
@@ -1,5 +1,53 @@
 #include <gtest/gtest.h>
 
+#include <functional>
+#include <memory>
+
+class Header
+{
+  public:
+    int m_cnt;
+    std::string m_name;
+  public:
+    void Dump()
+    {
+        std::cout<<"name:"<<m_name<<"  Cnt:"<<m_cnt<<std::endl;
+    }
+    Header()
+    {
+
+    }
+    void Print(const std::string& name,int cnt)
+    {
+      m_name = name;
+      m_cnt = cnt;
+
+      Dump();
+    }
+    ~Header()
+    {
+
+    }
+};
+
+
+
+template < typename... Args>
+void call_test(Args&&... args  )
+{
+  Header vl_hdr;
+  std::cout<<"Cnt:" << sizeof...(args) <<std::endl;
+  vl_hdr.Print(std::forward<Args>(args)...);
+  //return vl_hdr;
+} 
+
+
+
 TEST(Simple, simple) {
+  auto ptr = std::make_shared<Header>();
+  ptr->Dump();
+
+  call_test("123",5);
+
   ASSERT_EQ(1 + 1, 2);
 }
